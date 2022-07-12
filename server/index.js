340c119b5bc721419cf9f33804690617677a1209
Austin Mendoza <atmendoza1092@gmail.com>
2022-07-12 15:18:10 -0600

    fixing things

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import {
 import express from "express";
 import cors from "cors";
 const app = express();
-const port = process.env.PORT | 4000;
+const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
 
