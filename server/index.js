191a31696e7c9d032dbc5ce6803d351803302a6c
Austin Mendoza <atmendoza1092@gmail.com>
2022-07-12 15:08:19 -0600

    fixing things

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(4000, () => console.log("up on 4000"));
+app.listen(process.env.PORT | 4000, () => console.log("up on 4000"));
 
 app.get("/", (req, res) => {
   res.status(200).send();
